a8b31f242663be2e5cded8c9ff15bc4b37976a04
Peter Limbu <peterlimbu@spartaglobal.com>
2018-07-10 16:15:48 +0100

    modal finished

--- style/game.js
+++ style/game.js
@@ -1,32 +1,39 @@
 $(document).ready(function(){
   $('.img').on('dragstart', function(event) { event.preventDefault(); });
 
+var play = $(".play");
   var playerScore = 0;
   //set selectors
   var timer = $(".timer");
   var score = $(".score");
   var img = $(".img");
-
-
+  var imgSize = $(".size");
+ var restart = $(".restart");
+var timerPause = false;
 //TIMER
-var timeLeft = 120; //120 seconds timer for player
+var timeLeft = 30; //120 seconds timer for player
 setInterval( function countDown(){
+  if(timerPause ==true){
+
 timeLeft --;
+}
 if(timeLeft==0)
 {
-  alert("TIME IS UP BISH");
+  timeLeft =30;
+  playerScore = 0;
+  modal.style.display = "block";
+  clearInterval(interval);
 
-console.log(timeLeft);
 }
 else {
   timer.html("<b>Timer: "+ timeLeft+ " </b>");
-
 }
-
-
 }, 1000);
 
 
+
+
+var playGame = false;
 
 
 
@@ -47,7 +54,7 @@ function generateRandomForArray() {
 
 //generate random position values for top
 function generateRandom() {
-    var num = Math.floor(Math.random() * (($(window).height()-250) - 100 + 1) + 100   );
+    var num = Math.floor(Math.random() * (($(window).height()-250) - 150 + 1) + 150   );
     return num;
 }
 
@@ -57,18 +64,30 @@ function generateRandomLeft() {
     return num;
 }
 
+//random number generator to set size of characters
+function generateSize() {
+    var num = Math.floor(Math.random() *(500 - 100 + 1) + 100   );
+    return num;
+}
+//if statement needed here
 
 
+//sets value of imgaes and gos through them changing image file and position
 function setValue(){
-console.log(  $( window ).width());
+  if (playGame ==true)
+  {
+  $(".img img:last-child").remove()
 var num = container[generateRandomForArray()];
-
-$(".img").append("<img src ='img/"+ num+ ".png'>");
+var size = generateSize();
+img.append("<img class='size' style='width:"+size+"px' src ='img/"+ num+ ".png'>");
 var left = generateRandomLeft();
 var top = generateRandom();
-img.last().css({"position":"absolute", "top": top + "px", "left":left + "px"});
-setTimeout(function(){$(".img img:last-child").remove();
+img.last().css({"position":"absolute", "top": top + "px", "left":left + "px", "width":size+ "px","height":size+ "px"});
+/*
+setTimeout(function(){;
 }, 1000);
+*/
+}
 
 }
 
@@ -80,17 +99,58 @@ setTimeout(function(){$(".img img:last-child").remove();
 
 
 
-setInterval(function(){ setValue(); }, 1500);
 
 
 
+//clearInterval(interval);
+
+//clicking on image gets score and removes picture of enemy
 img.click(function(){
-
   playerScore++ ;
-
   score.html("<b>Score: "+ playerScore+ " </b>");
-
+$(".img img:last-child").remove()
 })
+
+
+//click function for modal to restart game
+restart.click(function(){
+location.reload();//reload page
+})
+
+var interval = setInterval(function(){ setValue();} , 1000);
+
+
+
+
+
+
+//play button
+play.click(function(){
+  if (playGame ==false)
+  {
+playGame = true;
+}else{
+  playGame = false;
+}
+if (timerPause ==false)
+{
+timerPause = true;
+}else{
+timerPause = false;
+}
+playerScore =0;
+timeLeft = 30;
+  //does my main function for popping image up in interval
+
+
+
+//close modal
+modal.style.display = "none";
+
+
+});
+
+
 
 
 
@@ -98,7 +158,6 @@ img.click(function(){
 //Modal
 
 
-// Get the modal
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
